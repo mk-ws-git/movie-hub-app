a6abbc981063a17cdd3821dc52cfff5171cf70db
mk-ws-git <maryksprague@gmail.com>
2026-02-10 10:33:53 +0100

    Add user and movie model

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    release_year = db.Column(db.Integer, nullable=False)
+
+    # Link movie to user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
