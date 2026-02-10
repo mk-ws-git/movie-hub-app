9f6bffe0a10cb0b77fb19da70505163bb01a975b
mk-ws-git <maryksprague@gmail.com>
2026-02-10 11:54:41 +0100

    Add configuration for SQLAlchemy and Flask

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import requests
+import os
+
 from flask import Flask
 from models import Movie
 from data_manager import DataManager
 
-OMDB_API_KEY = "YOUR_KEY"
+# Configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/movies.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+OMDB_API_KEY = os.getenv("OMDB_API_KEY")
+
+db.init_app(app)
+
 dm = DataManager()
 
 app = Flask(__name__)
