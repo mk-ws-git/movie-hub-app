eb46f92c6b4b557e2095c254f9b224c8e3ed97a9
mk-ws-git <maryksprague@gmail.com>
2026-02-10 11:03:35 +0100

    Enriched movies class and added user to movie relation table

--- models.py
+++ models.py
@@ -2,6 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+user_movie = db.Table(
+    "user_movie",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("movie_id", db.Integer, db.ForeignKey("movie.id"), primary_key=True),
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -10,7 +16,10 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     genre = db.Column(db.String(100), nullable=False)
-    release_year = db.Column(db.Integer, nullable=False)
-
-    # Link movie to user
+    year = db.Column(db.Integer, nullable=False)
+    director = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    country = db.Column(db.String(100), nullable=False)
+    poster_url = db.Column(db.String(100), nullable=False)
+    imdb_url = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
